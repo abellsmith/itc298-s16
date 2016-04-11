6d72f4533dc16aca864b5e3676b7c55193b92054
Abell Smith <abell.smith@thepnga.org>
2016-04-11 04:15:41 +0000

    Assignment 1 - Commit 1

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 var http = require("http");
 
- 
-
 http.createServer(function(req,res) {
 
-   res.writeHead(200, {'Content-Type': 'text/plain'});
+   var path = req.url.toLowerCase();    
 
-   res.end('Aloha world');
+   switch(path) {
+
+       case '/':
+
+           res.writeHead(200, {'Content-Type': 'text/plain'});
+
+           res.end('Home page');
+
+           break;
+
+       case '/about':
+
+           res.writeHead(200, {'Content-Type': 'text/plain'});
+
+           res.end('About page');
+
+           break;
+
+       default:
+
+           res.writeHead(404, {'Content-Type': 'text/plain'});
+
+           res.end('Not found');
+
+           break;
+
+   }    
 
 }).listen(process.env.PORT || 3000);
+
+console.log('Server started on localhost:3000; Press CTRL-C to terminate...');
